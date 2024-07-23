dcfd51b99daa5e7420cd6ef4fa4aba2923ebc1af
Ajinkya Anil Kahane <kahaneajinkya051@gmail.com>
2024-07-23 17:07:41 +0530

    feat: test fixes

--- src/test/init-test-server.ts
+++ src/test/init-test-server.ts
@@ -1,30 +1,14 @@
-import path from 'path';
-import dotenv from 'dotenv';
-const rootDir = path.resolve(__dirname, "../test");
-const pth = path.join(rootDir, '.env.test');
-dotenv.config({ path:  pth});
-
-import { PORT } from '../config';
-import { sequelize } from '../db';
-import { app } from '../providers';
-import { Community, Member, Role, User } from '../schema/v1';
-
+import { server } from "../server"
+import { Env } from '@loaders/v1';
 
 export const initTestServer = async () => {
   try {
-    const port = Number(PORT);
-    sequelize.addModels([User, Role, Community, Member]);
-
-    await sequelize.sync({ alter: true });
-
-    await sequelize.authenticate();
-    console.log('Connected to the test database');
-
-    const server = app.listen(port, () => {
-      console.log(`Express test server is listening on port ${port}`);
+    const app = await server()
+    const serverr = app.listen(Env.variable.NODE_PORT, () => {
+      console.log(`Express test server is listening on port ${Env.variable.NODE_PORT}`);
     });
 
-    return { app, server } ;
+    return { app, server: serverr } ;
   } catch (error) {
     console.error('Unable to connect to the test database:', error);
     throw error; // Throw error to indicate initialization failure
